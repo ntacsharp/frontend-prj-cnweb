1515dd6758332b8e9d51a27ecb1574599adbc2a0
ducaanhlehuu <lehuuducanh1@gmail.com>
2024-04-05 23:52:58 +0700

    Create Modal UI for Server and Setup UploadThing

---
+++ components/modals/initial_modal.tsx
@@ -0,0 +1,119 @@
+"use client";
+
+import * as z from "zod";
+import {zodResolver} from "@hookform/resolvers/zod"
+
+import {Dialog,DialogContent,DialogDescription,
+        DialogFooter,DialogHeader,DialogTitle} 
+from "@/components/ui/dialog"
+
+import { Form,FormControl,FormField,FormItem,FormLabel,FormMessage } from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { FileUpload } from "../file-upload";
+
+const formSchema = z.object({
+    name: z.string().min(1,{
+        message: "Tên máy chủ là bắt buộc."
+    }),
+    imageUrl: z.string().min(1, {
+        message: "Ảnh máy chủ là bắt buộc"
+    })
+})
+
+
+export const InitialModal = () =>{
+    const [isMounted,setIsMounted] = useState(false);
+    // Sử dụng để đảm bảo chỉ render khi đã mount component modal vào page
+
+    useEffect(() =>{
+        setIsMounted(true);
+    },[])
+
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            imageUrl: ""
+        }
+    })
+
+    const isLoading = form.formState.isSubmitting;
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) =>{
+        console.log(values);
+    };
+
+    if(!isMounted) {
+        return null;
+    }
+    return (
+        <Dialog open>
+            <DialogContent className="bg-white text-black overflow-hidden">
+                <DialogHeader className="pt-8 px-6">
+                   <DialogTitle className="text-center text-2xl font-bold">
+                   Tuỳ chỉnh máy chủ của bạn
+                   </DialogTitle>
+                   <DialogDescription className="text-center text-zinc-500">
+                   Tạo cho máy chủ của bạn một cái tên và ảnh. Bạn luôn có thể chỉnh sửa nó sau này
+                   </DialogDescription>
+                </DialogHeader>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} 
+                    className="space-y-8">
+                        <div className="space-y-8 px-6">
+                            <div className="flex items-center justify-center text-center">
+                                <FormField
+                                    control = {form.control}
+                                    name = "imageUrl"
+                                    render = {({field})=>(
+                                        <FormItem>
+                                            <FormControl>
+                                                <FileUpload 
+                                                    endpoint = "serverImageUploader"
+                                                    value = {field.value}
+                                                    onChange = {field.onChange}
+                                                />
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                />
+                            </div>
+                            <FormField
+                                control={form.control}
+                                name = "name"
+                                render = {({field})=>(
+                                    <FormItem>
+                                        <FormLabel 
+                                        className="text-xs text-zinc-500 font-bold uppercase">
+                                            Tên máy chủ
+                                        </FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                disabled={isLoading}
+                                                className = "bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0" 
+                                                placeholder = "Nhập tên máy chủ" 
+                                                {...field}
+                                            />  
+                                            
+                                        </FormControl>
+                                        <FormMessage/>
+                                    </FormItem>
+                                )}
+                            />
+                    
+                        </div>
+                        <DialogFooter className = "bg-gray-100 px-6 py-4" >
+                        <Button disabled={isLoading} className="bg-blue-600 text-white hover:bg-indigo-500/90">
+                            Tạo mới
+                        </Button>
+                    </DialogFooter>
+                    </form>
+                
+                </Form>
+            </DialogContent>
+        </Dialog>
+    )
+}
