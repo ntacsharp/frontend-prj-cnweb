70e033e3e073e269aad72ceaa0477d3a7ec97e05
hoanganh2070 <mhoanganh05@gmail.com>
2024-04-27 20:47:22 +0700

    Merge ducanh

---
+++ components/chat/chatheader.tsx
@@ -0,0 +1,26 @@
+import { Hash, Menu } from "lucide-react"
+import { MobileToggle } from "../responsive"
+
+interface header_props {
+    name:string,
+    type: "channel" | "conversation",
+    serverId: string,
+    imageUrl?: string
+}
+
+const ChatHeader =  ({serverId,name,type,imageUrl}:header_props) =>{
+  return (
+    <div className="text-md font-semibold flex items-center px-3 h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
+        {/* Nut toogle tren Mobile khi width giam */}
+      <MobileToggle serverID = {serverId}/>
+
+      {type==="channel" && (
+        <Hash className="w-5 h-5 mr-2 text-zinc-500 dark:text-zinc-400"/>
+      )}
+      <p className="font-semibold text-md text-black dark:text-white">
+        {name}
+      </p>
+    </div>
+  )
+}
+export default ChatHeader
