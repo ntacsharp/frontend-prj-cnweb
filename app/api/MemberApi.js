245306a0629dbfbf084d484ed04f9a58f1a5d7ee
ducaanhlehuu <lehuuducanh1@gmail.com>
2024-05-11 22:41:53 +0700

    Fix invite

--- app/api/MemberApi.js
+++ app/api/MemberApi.js
@@ -13,13 +13,11 @@ export const changeMemberRole = async (values, ids, token) => {
 }
 
 export const deleteMember = async (ids, token) => {
-    const payload = {
-        serverId: ids.serverId
-    };
+
     const headers = {
         Authorization: `Bearer ${token}`,
     };
-    return apiClient.delete(`/api/member/${ids.memberId}`, payload, {headers: headers});
+    return apiClient.delete(`/api/member/${ids.memberId}/${ids.serverId}`,{headers: headers});
 }
 
 export const getCurrentMember = async (serverId, token)  => {
