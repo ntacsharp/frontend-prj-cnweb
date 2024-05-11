af400b84eaeeacd845af6a91a1e2176a1831ede5
Tuấn Anh <92778429+ntacsharp@users.noreply.github.com>
2024-05-11 21:49:27 +0700

    Feat: Chat item

--- app/api/MessageApi.js
+++ app/api/MessageApi.js
@@ -11,3 +11,14 @@ export const sendMessage = async (token, url, value) => {
     const data = await apiClient.post(url, payload, { headers: headers });
     return data.data
 }
+
+export const editMessage = async (token, url, value) => {
+    const headers = {
+        Authorization: `Bearer ${token}`,
+    };
+    const payload = {
+        value: value.content
+    }
+    const data = await apiClient.patch(url, payload, { headers: headers });
+    return data.data
+}
