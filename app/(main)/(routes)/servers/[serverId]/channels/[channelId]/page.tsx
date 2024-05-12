b63084ad968de4b1b97a60b1d06225d7597af30d
Tuấn Anh <92778429+ntacsharp@users.noreply.github.com>
2024-05-12 19:11:37 +0700

    Feat: Video call

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -4,6 +4,7 @@ import { getCurrentMember } from "@/app/api/MemberApi";
 import ChatInput from "@/components/chat/chat-input";
 import { ChatMessages } from "@/components/chat/chat-message";
 import ChatHeader from "@/components/chat/chatheader";
+import { MediaRoom } from "@/components/media-room";
 import { Channel } from "@/model/Channel";
 import { ChannelType } from "@/model/ChannelType";
 import { Member } from "@/model/Member";
@@ -16,7 +17,7 @@ const ChannelIdPage = () => {
     const serverId = params?.serverId.toString()
     const [channelName, setChannelName] = useState("");
 
-    const [member, setMember] = useState<any>(); 
+    const [member, setMember] = useState<any>();
 
     const [channel, setChannel] = useState<Channel>({} as Channel);
 
@@ -31,7 +32,7 @@ const ChannelIdPage = () => {
 
 
                 const memberData = await getCurrentMember(serverId, sessionStorage.getItem('token'));
-                setMember(memberData); 
+                setMember(memberData);
 
             } catch (error) {
                 console.error("Error fetching channel:", error);
@@ -43,36 +44,46 @@ const ChannelIdPage = () => {
         }
     }, [params?.channelId]);
 
-    if(channelName!==""){
+    if (channelName !== "") {
         return (
             <div className="bg-white dark:bg-[#313338] flex flex-col h-screen">
                 <ChatHeader serverId={serverId} type="channel" name={channelName} />
-                <div className="flex-1"></div>
-                <ChatMessages
-                member={member}
-                name={channel.name}
-                chatId={channel.id}
-                type="channel"
-                apiUrl="/api/messages"
-                socketUrl="/api/socket/messages"
-                socketQuery={{
-                  channelId: channel.id,
-                  serverId: channel.serverId,
-                }}
-                paramKey="channelId"
-                paramValue={channel.id}
-              />
-                <ChatInput name={channelName} type="channel" apiUrl="/api/socket/messages" query={{
-                    channelId: params?.channelId,
-                    serverId: serverId
-                }} />
+                {/* <div className="flex-1"></div> */}
+                {channel.type === ChannelType.TEXT && (
+                    <>
+                        <ChatMessages
+                            member={member}
+                            name={channel.name}
+                            chatId={channel.id}
+                            type="channel"
+                            apiUrl="/api/messages"
+                            socketUrl="/api/socket/messages"
+                            socketQuery={{
+                                channelId: channel.id,
+                                serverId: channel.serverId,
+                            }}
+                            paramKey="channelId"
+                            paramValue={channel.id}
+                        />
+                        <ChatInput name={channelName} type="channel" apiUrl="/api/socket/messages" query={{
+                            channelId: params?.channelId,
+                            serverId: serverId
+                        }} />
+                    </>
+                )}
+                {channel.type === ChannelType.AUDIO && (
+                    <MediaRoom chatId={channel.id} video={false} audio={true} />
+                )}
+                {channel.type === ChannelType.VIDEO && (
+                    <MediaRoom chatId={channel.id} video={true} audio={true} />
+                )}
             </div>
         )
     }
 
     return (
         <div className="flex flex-col h-full w-full">
-            <Loader className="m-auto h-auto flex"/>
+            <Loader className="m-auto h-auto flex" />
         </div>
     )
 
