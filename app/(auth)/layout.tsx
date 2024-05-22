24b944dd84c68dcc43d07bea78351e40978b1572
hoanganh2070 <mhoanganh05@gmail.com>
2024-05-22 23:18:23 +0700

    Update Login/Register UI

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,7 +6,7 @@ export default function AuthLayout({
     children: ReactNode
 }){
     return (
-        <main className='w-full h-full bg-blue-300 mx-auto flex overflow-y-scroll'>
+        <main className=''>
             {children}
         </main>
     )
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -45,78 +45,52 @@ const Register = () => {
         }
     };
 
-    return (
-        <div className='mx-auto h-screen flex flex-col justify-center items-center'>
-            <div className='bg-gray-800 mx-auto w-[480px] p-8 rounded-lg'>
-                <div className="text-center">
-                    <h4 className="text-2xl font-bold">Đăng kí tài khoản Conserva.</h4>
-                    <hr className="my-4 border-t border-gray-300" />
-                </div>
-                <form onSubmit={handleSubmit} className='flex flex-col w-full'>
-                    <label htmlFor='username' className='text-white'>Username</label>
-                    <input 
-                        id='username' 
-                        type='text' 
-                        className='bg-gray-700 text-white border-b-2 border-gray-500 focus:outline-none focus:border-blue-500 py-2 px-3 rounded-lg mb-4' 
-                        value={username} 
-                        onChange={(e) => setUsername(e.target.value)} 
-                    />
-                    <label htmlFor='password' className='text-white'>Password</label>
-                    <input 
-                        id='password' 
-                        type='password' 
-                        className='bg-gray-700 text-white border-b-2 border-gray-500 focus:outline-none focus:border-blue-500 py-2 px-3 rounded-lg mb-4' 
-                        value={password} 
-                        onChange={(e) => setPassword(e.target.value)} 
-                    />
-                    <label htmlFor='email' className='text-white'>Email</label>
-                    <input 
-                        id='email' 
-                        type='email' 
-                        className='bg-gray-700 text-white border-b-2 border-gray-500 focus:outline-none focus:border-blue-500 py-2 px-3 rounded-lg mb-4' 
-                        value={email} 
-                        onChange={(e) => setEmail(e.target.value)} 
-                    />
-                    <label htmlFor='displayname' className='text-white'>Display name</label>
-                    <input 
-                        id='displayname' 
-                        type='text' 
-                        className='bg-gray-700 text-white border-b-2 border-gray-500 focus:outline-none focus:border-blue-500 py-2 px-3 rounded-lg mb-4' 
-                        value={displayName} 
-                        onChange={(e) => setDisplayName(e.target.value)} 
-                    />
-                    <button type="submit" className='bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg'>Sign Up</button>
-                </form>
-                {errorMessage && <p className="text-red-500">{errorMessage}</p>}
-                <br />
-                <Link href='/login'>
-                    <span className='text-blue-400 hover:text-blue-600 cursor-pointer'>Đăng nhập</span>
-                </Link>
-            </div>
-            {showModal && (
-                <div className="fixed top-0 left-0 flex items-center justify-center w-full h-full bg-gray-500 bg-opacity-75">
-                    <div className="bg-white p-12 rounded-lg relative">
-                        <button onClick={() => setShowModal(false)} className="absolute top-2 right-2 text-red-600 hover:text-red-700 focus:outline-none">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                            </svg>
-                        </button>
-                        <h2 className="text-3xl font-bold mb-4 text-zinc-950">Đăng ký thành công!</h2>
-                        <hr className="my-4 border-t border-gray-300" />
-                        {username && <p className="mb-4 text-zinc-950 font-bold">Tài khoản: {username}</p>}
-                        {email && <p className="mb-4 text-zinc-950 font-bold">Email: {email}</p>}
-                        <hr className="my-4 border-t border-gray-300" />
-                        <p className="mb-4 text-zinc-950">Chuyển đến trang đăng nhập?</p>
-                        <div className = " m-auto text-center mt-10 text-xl">
-                            <Link href="/login">
-                                <span className="bg-green-600 hover:bg-green-700 text-white px-4 py-3 rounded-lg">Đăng nhập</span>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-            )}
-        </div>
-    );
+        return (
+            <div >
+      <div style={{
+                    backgroundImage: "url('/background.webp')",
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    backgroundRepeat: 'no-repeat'
+                }} className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+              <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                  <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                      Đăng ký tài khoản Conversa
+                  </h1>
+                  <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6" >
+                      <div>
+                          <label htmlFor="email" className="block mb-2 text-sm font-bold text-gray-900 dark:text-indigo-300">Email</label>
+                          <input  value={email} 
+                        onChange={(e) => setEmail(e.target.value)}  type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
+                      </div>
+                      <div>
+                          <label htmlFor="username" className="block mb-2 text-sm font-bold text-gray-900 dark:text-indigo-300">Tên tài khoản</label>
+                          <input value={username} 
+                        onChange={(e) => setUsername(e.target.value)}  type="text" name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="username" required />
+                      </div>
+                      <div>
+                          <label htmlFor="displayName" className="block mb-2 text-sm font-bold text-gray-900 dark:text-indigo-300">Tên hiển thị</label>
+                          <input value={displayName} 
+                        onChange={(e) => setDisplayName(e.target.value)}  type="text" name="displayName" id="displayname" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="bexuanmailonton" required />
+                      </div>
+                      <div>
+                          <label htmlFor="password" className="block mb-2 text-sm font-bold text-gray-900 dark:text-indigo-300">Mật khẩu</label>
+                          <input value={password} 
+                        onChange={(e) => setPassword(e.target.value)} type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                      </div>
+                      {errorMessage && <p className="text-red-500 text-sm mt-0 mb-0">{errorMessage}</p>}
+                      <button type="submit" className="w-full text-white bg-indigo-500 hover:bg-indigo-600 duration-100 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-semibold rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Đăng ký</button>
+                      <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                          Bạn đã có tài khoản? <a href='/login' className="font-medium text-primary-600 hover:underline dark:text-primary-500">Đăng nhập</a>
+                      </p>
+                  </form>
+              </div>
+          </div>
+      </div>
+    </div>
+    
+        );
 };
 
 export default Register;
