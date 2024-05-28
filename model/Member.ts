e23943d2bc9ade3b6caa9d5c9b076f1aa5bb1f04
hoanganh <hoanganh@debian.hoanganh>
2024-05-28 21:26:34 +0700

    Fix some bugs

--- model/Member.ts
+++ model/Member.ts
@@ -1,13 +1,15 @@
 import { MemberRole } from "./MemberRole";
+import { Profile } from "./Profile";
 
 export class Member {
 
-    constructor(public id : string,public profileId : string, public serverId : string, public createdAt : Date, public updatedAt : Date, public role : MemberRole) {
+    constructor(public id : string,public profileId : string, public serverId : string, public createdAt : Date, public updatedAt : Date, public role : MemberRole, public profile : Profile) {
         this.id = id;
         this.profileId = profileId;
         this.serverId = serverId;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
         this.role = role;
+        this.profile = profile;
     }
 }
